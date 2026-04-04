4aeab6286c93c00050ec94f5696ecea62a0c6dda
narendra <narendragadde34@gmail.com>
2026-04-04 15:19:58 +0530

    fix: update api/src/server.js

--- api/src/server.js
+++ api/src/server.js
@@ -44,8 +44,8 @@ var app = express();
 app.disable('x-powered-by');
 app.use(cors());
 app.use(helmet());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ extended: true, limit: '20mb' }));
 
 var limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
